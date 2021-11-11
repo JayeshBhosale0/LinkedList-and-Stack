e6c7862655db433648481afa30c62113474e4635
Jayesh <jayesh.bhosale1812@gmail.com>
2021-11-11 18:35:34 +0530

    Day 14 problem added

---
+++ Operstion.java
@@ -0,0 +1,74 @@
+package practise;
+
+public class Operstion {
+	LinkedList linkedList = new LinkedList();
+
+	public void addDataAtStart() {
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		linkedList.print();
+	}
+
+	public void addDataAtEnd() {
+		linkedList.append(56);
+		linkedList.append(30);
+		linkedList.append(70);
+		linkedList.print();
+	}
+
+	public void addDataAtMiddle() {
+		Node secondNode = linkedList.push(70);
+		Node firstNode = linkedList.push(56);
+		Node newNode = new Node(30);
+		linkedList.print();
+		System.out.println("");
+		linkedList.insertInBetween(firstNode, newNode);
+		linkedList.print();
+	}
+
+	public void deleteFirstNode() {
+		addDataAtStart();
+		System.out.println("");
+		Node deleteNode = linkedList.pop();
+		linkedList.print();
+	}
+
+	public void deleteLastNode() {
+		addDataAtStart();
+		System.out.println("");
+		linkedList.popLast();
+		linkedList.print();
+
+	}
+
+	public void findNodeBasedOnValue() {
+		addDataAtStart();
+		linkedList.findNode(30);
+	}
+
+	public void insertDataAtGivenNode() {
+		addDataAtStart();
+		Node newNode = new Node(40);
+		Node previousNode = linkedList.searchNodeAt(30, newNode);
+		System.out.println("Before");
+		linkedList.print();
+	}
+
+	public void deleteAfterNodeAfterSearchNode() {
+		System.out.println("Before");
+		insertDataAtGivenNode();
+		System.out.println("after");
+		linkedList.deleteANode(40);
+		linkedList.print();
+	}
+
+	public void sortList() {
+		System.out.println("before sorting the list");
+		insertDataAtGivenNode();
+		linkedList.sort();
+		System.out.println("after sorting the list");
+		linkedList.print();
+	}
+
+}
